b3fb2c950c82aa021d82ce18fb628efc1e7fe2b5
ishwar4230 <atcikss9@kgpian.iitkgp.ac.in>
2025-03-19 23:50:05 +0530

    changed banner view in mobile

--- src/tabs/banner/Banner.js
+++ src/tabs/banner/Banner.js
@@ -1,8 +1,10 @@
 import React from "react";
 import {useNavigate} from 'react-router-dom';
 import "./BannerStyle.css";
+import { useMediaQuery } from "@mantine/hooks";
 
 const Banner = ({course_type, course_name}) => {
+    const isMobile = useMediaQuery("(max-width: 768px)"); // Detect mobile screens
     const navigate=useNavigate();
     return (
         <div className="whatsapp-banner">
@@ -10,7 +12,7 @@ const Banner = ({course_type, course_name}) => {
                 {/* <img src="LInk" alt="WhatsApp Icon" /> */}
                 <h3>Take a one time trial call with our IITian mentors now @ ₹49</h3>
             </div>
-            <button onClick={() => {navigate(`/checkout?course_type=${course_type}&course_name=${course_name}&plan_type=one_time`)}}  className="join-button">Take Trial</button>
+            <button onClick={() => {navigate(`/checkout?course_type=${course_type}&course_name=${course_name}&plan_type=one_time`)}}  className="join-button">{isMobile?'Book One-Time Trial @ ₹49':'Take Trial'}</button>
         </div>
     );
 };
